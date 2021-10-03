a3a31f04063c974a959a0bc51baf62bb8349c1db
sudarshanshenoy <shenoy.sudarshan@gmail.com>
2021-10-03 19:08:29 +0530

    add better solution for averagePair

--- Multipointer/averagePair.js
+++ Multipointer/averagePair.js
@@ -3,7 +3,7 @@ Write a function called averagePair, which accepts sorted interger array and a t
 Find out if there is pair of values in the array where the average of the pair equals the target average.
 */
 
-function averagePair(arr, target) {
+function averagePair(arr, target) {// O(n * m)
    
     if(arr.length === 0) {
       return false;
@@ -21,6 +21,32 @@ function averagePair(arr, target) {
 
         }
 
+    }
+
+    return false;
+}
+
+
+console.log(averagePair([1,2,3],2.5)) //true
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8)) //true
+console.log(averagePair([-1,0,3,4,5,6],4.1)) //false
+console.log(averagePair([],4)) //false
+
+
+function averagePair(arr, target) {  // better solution
+   
+    if(arr.length === 0) {
+      return false;
+    }
+    
+    let start = 0;
+    let end = arr.length - 1;
+
+    while(start < end) {
+      let avg = (arr[start] + arr[end]) / 2
+      if(avg === target) return true; 
+      else if(avg < target) start++
+      else end--
     }
 
     return false;
